bc74389e7feef38d78904fd0b6d346edac74d8f4
Oscar7890 <73807285+Oscar7890@users.noreply.github.com>
2023-03-22 00:37:33 -0600

    mejora al carrusel de imagenes
    se le corrigio unos errores que tenia

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
-var counter = 1;
+// var counter = 1;
 
-setInterval(function() {
+// setInterval(function() {
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+//     if(counter > 4) {
+//         counter = 1;
+//     }
+// }, 3000)
+
+var counter = 1;
+var intervalId = setInterval(function() {
     document.getElementById('radio' + counter).checked = true;
     counter++;
     if(counter > 4) {
         counter = 1;
     }
-}, 4000)
+}, 3000);
+
+var radioButtons = document.querySelectorAll('input[type="radio"]');
+radioButtons.forEach(function(radioButton) {
+    radioButton.addEventListener('click', function() {
+        clearInterval(intervalId);
+        counter = parseInt(this.id.replace('radio', ''));
+        intervalId = setInterval(function() {
+            document.getElementById('radio' + counter).checked = true;
+            counter++;
+            if(counter > 4) {
+                counter = 1;
+            }
+        }, 3000);
+    });
+});
+
+
+
+
+
