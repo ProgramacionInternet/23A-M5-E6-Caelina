1c2e27eaf7d99503696515a6e736c8d84f4a2fc0
Oscar7890 <73807285+Oscar7890@users.noreply.github.com>
2023-04-25 18:07:43 -0600

    Api del lado del cliente
    Se creo una api para sacar la geolocalizacion del dispositivo a traves del navegador y se uso en conjunto con la api de google maps

--- js/API-Maps.js
+++ js/API-Maps.js
@@ -1,11 +1,90 @@
-function iniciarMap(){
-    var coordenadas = {lat: 20.654861, lng: -103.32545};
-    var map = new google.maps.Map(document.getElementById("map"),{
-        center: coordenadas,
-        zoom: 15
-    });
-    var marker = new google.maps.Marker({
-        position: coordenadas,
-        map: map
-    });
+//////////// API local //////////////////////////
+
+// Verificar si el navegador soporta la geolocalización
+if ("geolocation" in navigator) {
+  // Obtener la ubicación del usuario
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      // Obtener las coordenadas de la ubicación
+      var latitud = position.coords.latitude;
+      var longitud = position.coords.longitude;
+
+      // Hacer algo con las coordenadas de la ubicación
+      // console.log("Latitud: " + latitud);
+      // console.log("Longitud: " + longitud);
+
+      iniciarMap(latitud, longitud);
+
+    },
+    function(error) {
+      // Manejar errores en la obtención de la ubicación
+      console.error("Error en la obtención de la ubicación: " + error.message);
+    },
+    {
+      enableHighAccuracy: true, // Habilitar alta precisión
+      timeout: 5000, // Tiempo máximo de espera en milisegundos
+      maximumAge: 0, // No utilizar caché de ubicaciones anteriores
+    }
+  );
+} else {
+   // Manejar el caso en que la geolocalización no esté disponible
+  console.error("La geolocalización no está disponible en este navegador.");
 }
+
+
+///////////  API de Tercero ////////////////////////////////
+
+function iniciarMap(latitud, longitud){
+  // var coordenadas = {lat: 20.654861, lng: -103.32545};
+  var coordenadas = {lat: latitud, lng: longitud};
+  var map = new google.maps.Map(document.getElementById("map"),{
+      center: coordenadas,
+      zoom: 12
+  });
+
+  // Array de ubicaciones con sus coordenadas
+  var ubicaciones = [
+    {
+      coordenadas: {lat: latitud, lng: longitud},
+    },
+    {
+      coordenadas: {lat: 20.654861, lng: -103.32545},
+      icono: '../Imagenes/Logo-icono.png'
+    },
+    {
+      coordenadas: {lat: 20.74081, lng: -103.31222},
+      icono: '../Imagenes/Logo-icono.png'
+    },
+    {
+      coordenadas: {lat: 20.74083, lng: -103.37976},
+      icono: '../Imagenes/Logo-icono.png'
+    },
+    {
+      coordenadas: {lat: 20.68739, lng: -103.33209},
+      icono: '../Imagenes/Logo-icono.png'
+    },
+    {
+      coordenadas: {lat: 20.56693, lng: -103.22852},
+      icono: '../Imagenes/Logo-icono.png'
+    },
+    {
+      coordenadas: {lat: 20.67640, lng: -103.34462},
+      icono: '../Imagenes/Logo-icono.png'
+    }
+    // Agrega más ubicaciones aquí
+  ];
+
+  // Iterar a través del array de ubicaciones y agregar marcadores en el mapa
+  ubicaciones.forEach(function(ubicaciones) {
+      var marker = new google.maps.Marker({
+          position: ubicaciones.coordenadas,
+          map: map,
+          icon: {
+              url: ubicaciones.icono,
+              scaledSize: new google.maps.Size(34, 34) // Tamaño deseado del icono
+          }
+      });
+  });
+}
+
+
--- js/app.js
+++ js/app.js
@@ -23,30 +23,7 @@ radioButtons.forEach(function(radioButton) {
 });
 
 
-// API local
 
-// Verificar si el navegador soporta la geolocalización
-if ("geolocation" in navigator) {
-    // Obtener la ubicación del usuario
-    navigator.geolocation.getCurrentPosition(
-      function(position) {
-        // Obtener las coordenadas de la ubicación
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        
-        // Hacer algo con las coordenadas de la ubicación
-        console.log("Latitud: " + latitude);
-        console.log("Longitud: " + longitude);
-      },
-      function(error) {
-        // Manejar errores en la obtención de la ubicación
-        console.error("Error en la obtención de la ubicación: " + error.message);
-      }
-    );
-  } else {
-    // Manejar el caso en que la geolocalización no esté disponible
-    console.error("La geolocalización no está disponible en este navegador.");
-  }
   
 
 
